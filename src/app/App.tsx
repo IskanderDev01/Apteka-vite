85d22b4c2f9da89a48215785f1262b9e5d93032c
IskanderDev01 <githubturabaevi01@gmail.com>
2024-08-10 13:55:45 +0500

    a

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,16 @@ const tg = window.Telegram.WebApp;
 function App() {
     
     useEffect(() => {
-        tg.ready();
+        // Проверяем, открыто ли приложение на мобильном устройстве
+        const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+        if (isMobile) {
+            // Можно перенаправить пользователя на другую страницу или показать сообщение
+            document.body.innerHTML = '<h1>Это приложение доступно только на компьютере</h1>';
+        } else {
+            // Если не мобильное устройство, делаем Telegram WebApp ready
+            tg.ready();
+        }
     }, []);
-
     return (
         <div className="min-h-screen bg-cover">
             <Suspense fallback="">
